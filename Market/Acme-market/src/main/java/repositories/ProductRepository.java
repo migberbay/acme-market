35447a6d398bf30dcc4a1d4d647ba1710b9dd388
Miguel Bermudo <digitalvodka95@gmail.com>
2019-08-27 20:00:25 +0200

    asdasd

--- Market/Acme-market/src/main/java/repositories/ProductRepository.java
+++ Market/Acme-market/src/main/java/repositories/ProductRepository.java
@@ -1,16 +1,19 @@
 
 package repositories;
 
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import domain.Curricula;
+import domain.Market;
 import domain.Product;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
-	@Query("select p.products. from Curricula c where c.provider.id=?1")
-	Collection<Product> findValidProductByMarket(Market market);
+	@Query("select m.request.product from Market m where m.request.status = ACCEPTED and m = ?1 and m.request.product.stock >0")
+	Collection<Product> findValidProductsByMarket(Market market);
 }
