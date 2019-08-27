8baa3eb04e8e987d747e5fc3450cf8f15cd01412
Urabity <parra2009juan@gmail.com>
2019-08-27 20:18:26 +0200

    Fixed query product

--- Market/Acme-market/src/main/java/repositories/ProductRepository.java
+++ Market/Acme-market/src/main/java/repositories/ProductRepository.java
@@ -15,7 +15,7 @@ import domain.Product;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 	
-	@Query("select p from Department d join d.market m where p.department.id=d.id and m.id=?1")
+	@Query("select p from Product p join p.department d join d.market m where p.department.id=d.id and m.id=?1")
 	Collection<Product> getProductsByMarket(int marketId);
 
 	@Query("select p from Product p where p.provider.id=?1")
